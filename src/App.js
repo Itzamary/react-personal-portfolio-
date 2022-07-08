2b918aa10d5ddc59c78681121864479a8f09f686
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-07-08 04:57:01 -0500

    created a Header componenet and imported it into the App.js file injected the header into the jsx of the app.js file and passed in hooks from the user state as props to the Header componenet. the header compnenet recieves the props and also passes the props to its chilldren component that is also imported.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import './App.css';
 
 
 function App() {
+
+  const [categories] = useState([
+    { name: "About Me"},
+    { name: "Portfolio"},
+    { name: "Contact"},
+    { name: "Resume"},
+  ]);
+  console.log(categories, 'app categories')
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  
   return (
     <div>
-      <Header></Header>
+      <Header
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Header>
       
     </div>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Nav from '../Nav'
 
 
-function Header() {
+function Header(props) {
+
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props
+    console.log(categories, 'header categories')
+
     return (
         <header>
             <h1>Itzamary Jimenez</h1>
+            <Nav 
+                categories={categories}
+                setCurrentCategory={setCurrentCategory}
+                currentCategory={currentCategory}
+            ></Nav>
         </header>
     )
 }
