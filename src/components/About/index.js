d409963a121ea3cf8d7b410b3d58f5a95c5ff767
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-07-17 22:48:32 -0500

    updated the paragraph tag in the jsx.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ function About(){
         <section className='my-5'>
             <img src={meImg} alt={'icon owner of this profile.'}></img>
 
-            <p>jlkasjdflkajsdlfkjalsdkfjalskdjflaksjdflkajsdflkajsldfkjas;ldkfjals;kdfjal;ksdjfl;aksjdflkasjdflkjaskdfjhaksjdhfkajshdfkjashdfkjahsdfkjasdklfjasdlkfjas;ldkfja;lskdjf;laksjdfl;aksjdf;lak</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget. Et sollicitudin ac orci phasellus egestas tellus rutrum. Enim facilisis gravida neque convallis a cras semper auctor neque. Tellus id interdum velit laoreet. Aliquet sagittis id consectetur purus. Libero id faucibus nisl tincidunt eget nullam non nisi. Facilisi etiam dignissim diam quis enim lobortis. Vitae semper quis lectus nulla. Elementum eu facilisis sed odio morbi quis commodo odio. Semper auctor neque vitae tempus quam pellentesque nec. Praesent elementum facilisis leo vel fringilla est. Velit laoreet id donec ultrices.</p>
         </section>
     )
 }
