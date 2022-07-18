c3a887b02da9e768f2ea2c5322833b5ff1ef1fa1
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-07-17 21:04:43 -0500

    imported and used a meimage in an image tag.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import meImg from "../../assets/about/meimg.png"
 
 
 function About(){
     return (
         <section className='my-5'>
+            <img src={meImg} alt={'icon owner of this profile.'}></img>
 
             <p>jlkasjdflkajsdlfkjalsdkfjalskdjflaksjdflkajsdflkajsldfkjas;ldkfjals;kdfjal;ksdjfl;aksjdflkasjdflkjaskdfjhaksjdhfkajshdfkjashdfkjahsdfkjasdklfjasdlkfjas;ldkfja;lskdjf;laksjdfl;aksjdf;lak</p>
         </section>
